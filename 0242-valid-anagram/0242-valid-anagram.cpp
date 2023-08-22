564f68155fbabc6a2590a181ff03ad8929bd56e1
Krishnansh Goswami <70022408+k-gos@users.noreply.github.com>
2023-08-22 23:45:13 +0530

    Time: 16 ms (27.28%), Space: 7.4 MB (47.03%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -5,10 +5,6 @@ public:
             return false;
         sort(s.begin(),s.end());
         sort(t.begin(),t.end());
-        // if(s.compare(t)==0)
-        //     return true;
-        // else
-        //     return false
         return(!s.compare(t));
     }
 };
