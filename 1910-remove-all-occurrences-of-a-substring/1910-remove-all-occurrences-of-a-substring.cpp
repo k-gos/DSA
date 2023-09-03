c54a1aea6a7c40c60dc380730a2e1ffbb3ff70dd
Krishnansh Goswami <70022408+k-gos@users.noreply.github.com>
2023-09-03 20:08:39 +0530

    Time: 6 ms (33.96%), Space: 6.6 MB (90.27%) - LeetHub

--- 1910-remove-all-occurrences-of-a-substring/1910-remove-all-occurrences-of-a-substring.cpp
+++ 1910-remove-all-occurrences-of-a-substring/1910-remove-all-occurrences-of-a-substring.cpp
@@ -1,23 +1,24 @@
 class Solution {
 public:
-    void solve(string&s,string &part)
-    {
-        if(s.find(part)==string::npos)
-            return;
-        int idx=s.find(part);
-        s.erase(idx,part.length());
-        solve(s,part);
-    }
+    // void solve(string&s,string &part)
+    // {
+    //     if(s.find(part)==string::npos)
+    //         return;
+    //     int idx=s.find(part);
+    //     s.erase(idx,part.length());
+    //     solve(s,part);
+    // }
     string removeOccurrences(string s, string part) {
-//         while(s.find(part)!=string::npos)
-//         {
-//             int idx=s.find(part);
-//             s.erase(idx,part.length());
-//         }  
+        while(s.find(part)!=string::npos)
+        {
+            int idx=s.find(part);
+            s.erase(idx,part.length());
+        }  
         
-//         return s;
-//         Recursive approach
-        solve(s,part);
         return s;
+        
+        //Recursive approach
+        // solve(s,part);
+        // return s;
     }
 };
