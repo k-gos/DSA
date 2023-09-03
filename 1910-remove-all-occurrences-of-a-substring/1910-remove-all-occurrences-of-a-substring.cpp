697e614440245c0168b988699423c58d968ac4c2
Krishnansh Goswami <70022408+k-gos@users.noreply.github.com>
2023-09-03 20:07:56 +0530

    Time: 3 ms (73.09%), Space: 6.6 MB (90.27%) - LeetHub

--- 1910-remove-all-occurrences-of-a-substring/1910-remove-all-occurrences-of-a-substring.cpp
+++ 1910-remove-all-occurrences-of-a-substring/1910-remove-all-occurrences-of-a-substring.cpp
@@ -1,13 +1,23 @@
 class Solution {
 public:
+    void solve(string&s,string &part)
+    {
+        if(s.find(part)==string::npos)
+            return;
+        int idx=s.find(part);
+        s.erase(idx,part.length());
+        solve(s,part);
+    }
     string removeOccurrences(string s, string part) {
-        int l=s.length();
-        while(s.find(part)!=string::npos)
-        {
-            int idx=s.find(part);
-            s.erase(idx,part.length());
-        }  
+//         while(s.find(part)!=string::npos)
+//         {
+//             int idx=s.find(part);
+//             s.erase(idx,part.length());
+//         }  
         
+//         return s;
+//         Recursive approach
+        solve(s,part);
         return s;
     }
 };
